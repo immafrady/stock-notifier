b6374c90146293326cdf992d3ef7397ce41b7803
Frady Wang <immafrady@gmail.com>
2024-11-27 11:25:40 +0800

    feat: 调整表格查询样式

--- go.mod
+++ go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/marcozac/go-jsonc v0.1.1
 	github.com/shopspring/decimal v1.4.0
 	github.com/spf13/cobra v1.8.1
-	golang.org/x/text v0.19.0
+	golang.org/x/text v0.20.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
--- internal/search/infos.go
+++ internal/search/infos.go
@@ -2,6 +2,7 @@ package search
 
 import (
 	"github.com/jedib0t/go-pretty/v6/table"
+	"golang.org/x/text/width"
 	"regexp"
 )
 
@@ -37,12 +38,48 @@ func (s StockInfos) String() string {
 	tw.SetTitle(s.Condition)
 	tw.AppendHeader(table.Row{"查询代码", "名称", "缩写", "所属市场", "分类"})
 	rows := make([]table.Row, len(s.StockInfos))
+	// 获取每个格子最长的一格
+	var mSearchCode, mName, mAbbr, mExchange, mClassify int
+	for _, info := range s.StockInfos {
+		updateMaxWidth(calcWidth(info.SearchCode()), &mSearchCode)
+		updateMaxWidth(calcWidth(info.Name), &mName)
+		updateMaxWidth(calcWidth(info.Abbr), &mAbbr)
+		updateMaxWidth(calcWidth(info.Exchange), &mExchange)
+		updateMaxWidth(calcWidth(info.Classify), &mClassify)
+	}
+	const additionWidth = 5
+	tw.SetColumnConfigs([]table.ColumnConfig{
+		{Number: 1, WidthMin: mSearchCode + additionWidth},
+		{Number: 2, WidthMin: mName + additionWidth},
+		{Number: 3, WidthMin: mAbbr + additionWidth},
+		{Number: 4, WidthMin: mExchange + additionWidth},
+		{Number: 5, WidthMin: mClassify + additionWidth},
+	})
 	for i, info := range s.StockInfos {
 		rows[i] = table.Row{info.SearchCode(), info.Name, info.Abbr, info.Exchange, info.Classify}
 	}
 	tw.AppendRows(rows)
 	tw.SetAutoIndex(true)
 	tw.SetCaption("\n\n")
-	tw.SetStyle(table.StyleBold)
+	tw.SetStyle(table.StyleColoredRedWhiteOnBlack)
 	return tw.Render()
+}
+
+// 计算宽度相关
+func calcWidth(s string) (w int) {
+	for _, r := range s {
+		prop := width.LookupRune(r)
+		if prop.Kind() == width.EastAsianFullwidth || prop.Kind() == width.EastAsianWide {
+			w += 2
+		} else {
+			w += 1
+		}
+	}
+	return
+}
+
+func updateMaxWidth(curr int, target *int) {
+	if curr > *target {
+		*target = curr
+	}
 }
